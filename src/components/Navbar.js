62f761cf768483ff9f8c78ac2c53ebc5e59b0c1a
Devyash <bordia999@gmail.com>
2020-06-25 15:59:55 +0530

    kya kismat hai bc

---
+++ src/components/Navbar.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+    StyledNavbar,
+    StyledNavBrand,
+    StyledNavItems,
+    StyledLink,
+} from "../styled/Navbar";
+import { Accent } from "../styled/Random";
+
+export default function Navbar() {
+    return (
+        <StyledNavbar>
+            <StyledNavBrand>
+                <Link to="/">
+                    Learn.Build.<Accent>Type.</Accent>
+                </Link>
+            </StyledNavBrand>
+            <StyledNavItems>
+                <li>
+                    <StyledLink to="/">Home</StyledLink>
+                </li>
+                <li>
+                    <StyledLink to="/highScores">High Scores</StyledLink>
+                </li>
+            </StyledNavItems>
+        </StyledNavbar>
+    );
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import CTA from "../styled/CTA";
+import { Accent, StyledTitle } from "../styled/Random";
+
+export default function Home() {
+    return (
+        <div>
+            <StyledTitle>Ready to Type?</StyledTitle>
+            <CTA to="/game">
+                Click or type <Accent>'s'</Accent> to start playing
+            </CTA>
+        </div>
+    );
+}
