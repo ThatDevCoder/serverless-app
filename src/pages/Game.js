b024ea48bd0d25802f1c9e5c960cf6c9c725ea80
Devyash <bordia999@gmail.com>
2020-06-27 17:42:36 +0530

    callback added that was breaking code

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -24,6 +24,7 @@ export default function Game({ history }) {
         return () => {
             clearInterval(interval);
         };
+        // eslint-disable-next-line
     }, []);
 
     const updateTime = (startTime) => {
@@ -69,6 +70,7 @@ export default function Game({ history }) {
             }
             setRandomCharacter();
         },
+        // eslint-disable-next-line
         [currentCharacter]
     );
 
